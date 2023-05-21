5aea2045786f25e925cdfa4f4dbd8d0cb2dce09c
AugmenTab <tbaum92@live.com>
2023-05-20 21:41:39 -0500

    Adds documentation and convenience functions to Scale modules
    This adds more Haddock documentation to `Data.Music.Scale` and
    `Data.Music.Scales`, as well as more helper functions to do things
    like add `Interval`s to a `Scale`, and find the union, difference,
    and intersection of `Scale`s. We're also now attempting to dedupe
    `Scale`s when building them, and as a result, there should be no
    chance of failure since a complete list of intervals that are all
    a different number of half-steps from the tonic would be the
    chromatic scale. This is due to a bug in the `Ord` instance of
    `Interval` which will be fixed in the next commit.

--- src/Data/Music/Scale.hs
+++ src/Data/Music/Scale.hs
@@ -1,9 +1,23 @@
 module Data.Music.Scale
-  ( Scale(..)
+  ( Scale
+     ( Ditonic
+     , Tritonic
+     , Tetratonic
+     , Pentatonic
+     , Hexatonic
+     , Heptatonic
+     , Octatonic
+     , Nonatonic
+     , Decatonic
+     , Undecatonic
+     , Chromatic
+     )
   , Mode
   , scaleFromIntervals
   , scaleToIntervals
   , scaleToText
+  , addInterval
+  , mergeScales, scaleDifference, scaleIntersection, scaleUnion
   ) where
 
 import           Flipstone.Prelude
@@ -13,13 +27,20 @@ import           Data.Music.Invertible (Invertible(..))
 import qualified Data.Foldable as F
 import qualified Data.FixedList as FL
 import qualified Data.List as L
+import qualified Data.Set as Set
 import qualified Data.Text as T
 import           Text.Show (Show(..))
 
--- A Scale is a list of fixed length, holding the interval values to determine
+-- | A Scale is a list of fixed length, holding the Interval values to determine
 -- the 2nd through last note in the scale from the tonic. They all have a fixed
 -- length of 1 less than the scale length - we don't need to determine distance
--- from the tonic for the tonic or the octave.
+-- from the tonic for the tonic or the octave. They are always sorted into
+-- ascending order, and are effectively a 'Set'.
+--
+-- Scales have a notion of enharmonic equivalence; two scales of equal size
+-- where each interval in the same position of each scale both have an equal
+-- number of half steps from the tonic will be treated as equivalent.
+--
 data Scale
   = Ditonic     (FL.FixedList1  I.Interval)
   | Tritonic    (FL.FixedList2  I.Interval)
@@ -57,25 +78,44 @@ instance Invertible Scale where
 instance Show Scale where
   show = T.unpack . scaleToText
 
--- This type synonym is a convenience for using a mode.
+-- | Type synonym for using a mode.
 type Mode = Scale
 
+-- | Attempts to build a Scale from a provided list of Intervals. Because Scales
+-- are represented with the tonic as implicit, the list of Intervals provided
+-- should be of a size 1 less than the desired Scale. So, to get the Major
+-- scale, one would provide a list of six Intervals containing the Intervals
+-- that build the 2nd through the 7th scale degrees.
+--
+-- Will fail if the provided list has 11 or more Intervals (the Chromatic scale
+-- would have 11 Intervals, with the 12th being the implicit tonic).
+--
 scaleFromIntervals :: [I.Interval] -> Either T.Text Scale
 scaleFromIntervals intervals =
+  -- TODO: There's no reason this should fail if using dedupeAndSort. The
+  -- problem here is the Ord instance for Interval does not remove equivalent
+  -- Intervals because they are ordered afterward based on their quality and
+  -- size The problem here is the Ord instance for Interval does not remove
+  -- equivalent Intervals because they are ordered after checking their half
+  -- steps based on their quality and size.
   case L.length intervals of
-    2  -> Right $ Ditonic     $ FL.fromFoldable' intervals
-    3  -> Right $ Tritonic    $ FL.fromFoldable' intervals
-    4  -> Right $ Tetratonic  $ FL.fromFoldable' intervals
-    5  -> Right $ Pentatonic  $ FL.fromFoldable' intervals
-    6  -> Right $ Hexatonic   $ FL.fromFoldable' intervals
-    7  -> Right $ Heptatonic  $ FL.fromFoldable' intervals
-    8  -> Right $ Octatonic   $ FL.fromFoldable' intervals
-    9  -> Right $ Nonatonic   $ FL.fromFoldable' intervals
-    10 -> Right $ Decatonic   $ FL.fromFoldable' intervals
-    11 -> Right $ Undecatonic $ FL.fromFoldable' intervals
-    12 -> Right $ Chromatic   $ FL.fromFoldable' intervals
-    l  -> Left $ T.pack $ "Cannot build a scale of length " <> show l
+    1  -> Right . Ditonic     . FL.fromFoldable' $ dedupeAndSort intervals
+    2  -> Right . Tritonic    . FL.fromFoldable' $ dedupeAndSort intervals
+    3  -> Right . Tetratonic  . FL.fromFoldable' $ dedupeAndSort intervals
+    4  -> Right . Pentatonic  . FL.fromFoldable' $ dedupeAndSort intervals
+    5  -> Right . Hexatonic   . FL.fromFoldable' $ dedupeAndSort intervals
+    6  -> Right . Heptatonic  . FL.fromFoldable' $ dedupeAndSort intervals
+    7  -> Right . Octatonic   . FL.fromFoldable' $ dedupeAndSort intervals
+    8  -> Right . Nonatonic   . FL.fromFoldable' $ dedupeAndSort intervals
+    9  -> Right . Decatonic   . FL.fromFoldable' $ dedupeAndSort intervals
+    10 -> Right . Undecatonic . FL.fromFoldable' $ dedupeAndSort intervals
+    11 -> Right . Chromatic   . FL.fromFoldable' $ dedupeAndSort intervals
+    l  ->
+      Left
+        . T.pack
+        $ "Cannot build a scale from " <> show l <> " intervals."
 
+-- | Unpacks a Scale into a list of its Intervals.
 scaleToIntervals :: Scale -> [I.Interval]
 scaleToIntervals (Ditonic     intervals) = F.toList intervals
 scaleToIntervals (Tritonic    intervals) = F.toList intervals
@@ -110,3 +150,42 @@ scaleToText scale =
         Decatonic   _ -> "Decatonic "   <> intervals
         Undecatonic _ -> "Undecatonic " <> intervals
         Chromatic   _ -> "Chromatic "   <> intervals
+
+-- | Attempts to add an Interval to a provided Scale. This will de-dupe the
+-- Scale to remove equivalent intervals. Will fail if attempting to build a
+-- Scale with 12 or more Intervals.
+addInterval :: I.Interval -> Scale -> Either T.Text Scale
+addInterval interval =
+  scaleFromIntervals
+    . dedupeAndSort
+    . (:) interval
+    . scaleToIntervals
+
+-- | Merges two Scales together, effectively taking the union of the two. Will
+-- fail if the resulting Scale has 12 or more Intervals.
+mergeScales :: Scale -> Scale -> Either T.Text Scale
+mergeScales scale1 scale2 = scaleFromIntervals $ scaleUnion scale1 scale2
+
+-- | Finds the Intervals that are not in common between two provided Scales.
+scaleDifference :: Scale -> Scale -> [I.Interval]
+scaleDifference scale1 scale2 =
+  Set.toAscList $ Set.difference (scaleSet scale1) (scaleSet scale2)
+
+-- | Finds the Intervals in common between two provided Scales.
+scaleIntersection :: Scale -> Scale -> [I.Interval]
+scaleIntersection scale1 scale2 =
+  Set.toAscList $ Set.intersection (scaleSet scale1) (scaleSet scale2)
+
+-- | Finds the unique Intervals across two provided Scales.
+scaleUnion :: Scale -> Scale -> [I.Interval]
+scaleUnion scale1 scale2 =
+  Set.toAscList $ Set.union (scaleSet scale1) (scaleSet scale2)
+
+--
+-- Helpers
+--
+dedupeAndSort :: [I.Interval] -> [I.Interval]
+dedupeAndSort = Set.toAscList . Set.fromList
+
+scaleSet :: Scale -> Set.Set I.Interval
+scaleSet = Set.fromList . scaleToIntervals
--- src/Data/Music/Scales.hs
+++ src/Data/Music/Scales.hs
@@ -34,35 +34,43 @@ import           Data.FixedList (fromFoldable')
 --
 -- Common Scales
 --
+
+-- | The major scale.
 major :: Scale
 major =
   Heptatonic
     $ fromFoldable' [ I.maj_2, I.maj_3, I.per_4, I.per_5, I.maj_6, I.maj_7 ]
 
+-- | The natural minor scale.
 minor :: Scale
 minor =
   Heptatonic
     $ fromFoldable' [ I.maj_2, I.min_3, I.per_4, I.per_5, I.min_6, I.min_7 ]
 
+-- | The harmonic minor scale.
 harmonicMinor :: Scale
 harmonicMinor =
   Heptatonic
     $ fromFoldable' [ I.maj_2, I.min_3, I.per_4, I.per_5, I.min_6, I.maj_7 ]
 
--- This is melodic minor ascending; descending is just natural minor.
+-- | The melodic minor scale (ascending).
 melodicMinor :: Scale
 melodicMinor =
   Heptatonic
     $ fromFoldable' [ I.maj_2, I.min_3, I.per_4, I.per_5, I.maj_6, I.maj_7 ]
 
+-- | The (hexatonic) blues scale, built from the minor pentatonic scale plus the
+-- flat 5th degree.
 blues :: Scale
 blues =
   Hexatonic $ fromFoldable' [ I.min_3, I.per_4, I.dim_5, I.per_5, I.min_7 ]
 
+-- | The whole-tone scale.
 wholeTone :: Scale
 wholeTone =
   Hexatonic $ fromFoldable' [ I.maj_2, I.maj_3, I.aug_4, I.aug_5, I.min_7 ]
 
+-- | The octatonic scale built using alternating half- and whole-step intervals.
 octatonicHW :: Scale
 octatonicHW =
   Octatonic
@@ -76,6 +84,7 @@ octatonicHW =
         , I.min_7
         ]
 
+-- | The octatonic scale built using alternating whole- and half-step intervals.
 octatonicWH :: Scale
 octatonicWH =
   Octatonic
@@ -89,6 +98,7 @@ octatonicWH =
         , I.maj_7
         ]
 
+-- | The chromatic scale.
 chromatic :: Scale
 chromatic =
   Chromatic
@@ -109,14 +119,18 @@ chromatic =
 --
 -- Pentatonic Scales
 --
+
+-- | The major pentatonic scale.
 pentatonicMajor :: Scale
 pentatonicMajor =
   Pentatonic $ fromFoldable' [ I.maj_2, I.maj_3, I.per_5, I.maj_6 ]
 
+-- | The minor pentatonic scale.
 pentatonicMinor :: Scale
 pentatonicMinor =
   Pentatonic $ fromFoldable' [ I.min_3, I.per_4, I.per_5, I.min_7 ]
 
+-- | The neutral pentatonic scale.
 pentatonicNeutral :: Scale
 pentatonicNeutral =
   Pentatonic $ fromFoldable' [ I.maj_2, I.per_4, I.per_5, I.min_7 ]
@@ -124,32 +138,42 @@ pentatonicNeutral =
 --
 -- Church Modes
 --
+
+-- | The Ionian mode, the first mode of the major scale. This is equivalent to
+-- the major scale.
 ionian :: Scale
 ionian = major
 
+-- | The Dorian mode, the second mode of the major scale.
 dorian :: Scale
 dorian =
   Heptatonic
     $ fromFoldable' [ I.maj_2, I.min_3, I.per_4, I.per_5, I.maj_6, I.min_7 ]
 
+-- | The Phrygian mode, the third mode of the major scale.
 phrygian :: Scale
 phrygian =
   Heptatonic
     $ fromFoldable' [ I.min_2, I.min_3, I.per_4, I.per_5, I.min_6, I.min_7 ]
 
+-- | The Lydian mode, the fourth mode of the major scale.
 lydian :: Scale
 lydian =
   Heptatonic
     $ fromFoldable' [ I.maj_2, I.maj_3, I.aug_4, I.per_5, I.maj_6, I.maj_7 ]
 
+-- | The Mixolydian mode, the fifth mode of the major scale.
 mixolydian :: Scale
 mixolydian =
   Heptatonic
     $ fromFoldable' [ I.maj_2, I.maj_3, I.per_4, I.per_5, I.maj_6, I.min_7 ]
 
+-- | The Aeolian mode, the sixth mode of the major scale. This is equivalent to
+-- the natural minor scale.
 aeolian :: Scale
 aeolian = minor
 
+-- | The Locrian mode, the seventh and final mode of the major scale.
 locrian :: Scale
 locrian =
   Heptatonic
